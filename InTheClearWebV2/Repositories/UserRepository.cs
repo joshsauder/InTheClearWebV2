4b8dfb64cb5eb59844bcc18a6a54c78f9f894227
Josh Sauder <joshsauder@gmail.com>
2020-01-12 15:34:01 -0500

    added user repository

---
+++ InTheClearWebV2/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using InTheClearWebV2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InTheClearWebV2.Repositories
+{
+    public class UserRepository : DbContext
+    {
+        public UserRepository(DbContextOptions<UserRepository> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> locations { get; set; }
+    }
+}
