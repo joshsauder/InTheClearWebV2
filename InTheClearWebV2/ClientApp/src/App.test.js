f3309af6de739a7459e6eae514ae5be94194a135
Josh Sauder <joshsauder@gmail.com>
2020-03-06 22:45:43 -0500

    fixed app tests for redux

--- InTheClearWebV2/ClientApp/src/App.test.js
+++ InTheClearWebV2/ClientApp/src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'jest-enzyme'
+
+import { createStore } from 'redux'
+
+let store = createStore(jest.fn())
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<App store={store} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
